ffbd008467970e237b52272eea657d9383cf0a0f
kaii <imkaiilb@gmail.com>
2024-06-19 19:02:24 +0300

    git am and back to apply, again + patches

--- patches/apply.sh
+++ patches/apply.sh
@@ -15,7 +15,8 @@ for project in $(cd "$patches"/"$tree"; echo *); do
     [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
     [ "$p" == vendor/partner/gms ] && p=vendor/partner_gms
     pushd "$p" &>/dev/null
-    git am --abort
+    # git am --abort
+    # rm -rf .git/rebase-apply
     for patch in "$patches"/"$tree"/"$project"/*.patch; do
         git apply "$patch" -v || exit
     done
