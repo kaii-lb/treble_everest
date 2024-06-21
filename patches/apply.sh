e0d2a2827c5eecf05ec88c0be10a956d70027ec4
kaii <imkaiilb@gmail.com>
2024-06-21 21:06:49 +0300

    updated patches from ponces's treble_aosp

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,24 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
-# patches=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-patches=$(realpath treblestuff/patches)
+patches="$(readlink -f -- $1)"
 tree="$2"
+patches="${patches}/treblestuff"
 
-echo "Applying ${tree} patches:"
+echo patchesDir $patches
 
-for project in $(cd "$patches"/"$tree"; echo *); do
-    p="$(tr _ / <<<"$project" |sed -e 's;platform/;;g')"
+for project in $(cd $patches/patches/$tree; echo *); do
+    p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
     [ "$p" == build ] && p=build/make
     [ "$p" == treble/app ] && p=treble_app
-    [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
-    [ "$p" == vendor/partner/gms ] && p=vendor/partner_gms
-    pushd "$p" &>/dev/null
-    # git am --abort
-    # rm -rf .git/rebase-apply
-    for patch in "$patches"/"$tree"/"$project"/*.patch; do
-        git apply "$patch" -v || exit
+    [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay    
+    pushd $p &>/dev/null
+    for patch in $patches/patches/$tree/$project/*.patch; do
+        git am $patch -3 && echo "succeeded" || exit
     done
     popd &>/dev/null
 done
