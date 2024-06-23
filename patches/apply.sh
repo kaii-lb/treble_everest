219b2b886f0c3c0d974d8869e4fcaa50b3ee8d15
kaii <imkaiilb@gmail.com>
2024-06-23 11:21:20 +0300

    added echo

--- patches/apply.sh
+++ patches/apply.sh
@@ -13,6 +13,7 @@ for project in $(cd $patches/patches/$tree; echo *); do
     [ "$p" == build ] && p=build/make
     [ "$p" == treble/app ] && p=treble_app
     [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay    
+    echo directory is $p
     pushd $p &>/dev/null
     for patch in $patches/patches/$tree/$project/*.patch; do
         git am $patch -3 && echo "succeeded" || exit
