a97a74b0d2ad9a6282f83ea26184d9cc52346d5c
kaii <imkaiilb@gmail.com>
2024-07-16 16:25:44 +0300

    added support for applying only a specific set of patches

--- patches/apply.sh
+++ patches/apply.sh
@@ -8,14 +8,12 @@ patches="${patches}/treblestuff"
 
 echo patchesDir $patches
 
-for project in $(cd $patches/patches/$tree; echo *); do
-    p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
-    [ "$p" == build ] && p=build/make
-    [ "$p" == treble/app ] && p=treble_app
-    [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay    
-    echo directory is $p
-    pushd $p &>/dev/null
-    for patch in $patches/patches/$tree/$project/*.patch; do
+if [[ $string == *"/"* ]]; then
+	cutDir=$(echo $tree | cut -d"/" -f2)
+	echo $neededDir
+	cdDir=$(tr _ / <<<$project |sed -e 's;platform/;;g')
+	pushd $cdDir &>/dev/null
+	for patch in $patches/patches/$tree/*.patch; do
         if git am $patch; then
        	   echo "PATCH ${project} SUCCEEDED!"
        	else
@@ -24,4 +22,23 @@ for project in $(cd $patches/patches/$tree; echo *); do
      	fi
     done
     popd &>/dev/null
-done
+else
+	for project in $(cd $patches/patches/$tree; echo *); do
+	    p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
+	    [ "$p" == build ] && p=build/make
+	    [ "$p" == treble/app ] && p=treble_app
+	    [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay    
+	    echo directory is $p
+	    pushd $p &>/dev/null
+	    for patch in $patches/patches/$tree/$project/*.patch; do
+	        if git am $patch; then
+	       	   echo "PATCH ${project} SUCCEEDED!"
+	       	else
+	       	   echo "PATCH ${project} FAILED, skipping..."
+	       	   git am --skip
+	     	fi
+	    done
+	    popd &>/dev/null
+	done
+fi
+
