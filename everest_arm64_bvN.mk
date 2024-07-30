e0c97563bb0af580516eada350074d58b30749fe
kaii <imkaiilb@gmail.com>
2024-07-31 00:41:44 +0300

    added bvn and bgn make files

---
+++ everest_arm64_bvN.mk
@@ -0,0 +1,20 @@
+TARGET_GAPPS_ARCH := arm64
+include build/make/target/product/aosp_arm64.mk
+$(call inherit-product, device/phh/treble/base.mk)
+
+
+$(call inherit-product, device/phh/treble/everest.mk)
+
+PRODUCT_NAME := everest_arm64_bvN
+PRODUCT_DEVICE := tdgsi_arm64_ab
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+PRODUCT_MODEL := EverestOS Vanilla
+
+WITH_GAPPS := false
+
+# Overwrite the inherited "emulator" characteristics
+PRODUCT_CHARACTERISTICS := device
+
+PRODUCT_PACKAGES += \
+	org.lineageos.glimpse
