c51fca32cf1905c3169ada9ae75282a129937ed9
kaii <imkaiilb@gmail.com>
2024-08-04 14:38:28 +0300

    fixed using phhgapps with mindthegapps

--- everest_arm64_bvN.mk
+++ everest_arm64_bvN.mk
@@ -2,7 +2,6 @@ TARGET_GAPPS_ARCH := arm64
 include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/phh/treble/base.mk)
 
-
 $(call inherit-product, device/phh/treble/everest.mk)
 
 PRODUCT_NAME := everest_arm64_bvN
@@ -12,6 +11,9 @@ PRODUCT_SYSTEM_BRAND := google
 PRODUCT_MODEL := EverestOS Vanilla
 
 WITH_GAPPS := false
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.system.ota.json_url=https://raw.githubusercontent.com/kaii-lb/treble_everest/14/ota.json
 
 # Overwrite the inherited "emulator" characteristics
 PRODUCT_CHARACTERISTICS := device
